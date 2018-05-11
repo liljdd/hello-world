539c6a8bbb8f8d9545e4e665c8dff3de61803984
lilj <lilj@mchina.cn>
2018-05-11 18:08:46 +0800

    update02

--- src/com/demo/HelloWorld.java
+++ src/com/demo/HelloWorld.java
@@ -3,6 +3,6 @@ package com.demo;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!!!");
-        System.out.println("good!");
+        System.out.println("github is good!");
     }
 }
