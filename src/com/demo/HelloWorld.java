3e1db3be73c72f0ad0f5d673b57c899fc9cb11f6
lilj <lilj@mchina.cn>
2018-05-14 10:27:01 +0800

    update05

--- src/com/demo/HelloWorld.java
+++ src/com/demo/HelloWorld.java
@@ -3,7 +3,7 @@ package com.demo;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!!!");
-        System.out.println("github is good!");
         System.out.println("commit and push");
+        System.out.println("中文乱码？！");
     }
 }
