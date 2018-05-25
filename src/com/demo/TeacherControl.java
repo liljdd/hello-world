123a36403ed3f97c3ffeb904e801fdfe7170094c
ant_mu <36785897+liljdd@users.noreply.github.com>
2018-05-25 14:31:38 +0800

    Update TeacherControl.java

--- src/com/demo/TeacherControl.java
+++ src/com/demo/TeacherControl.java
@@ -15,7 +15,7 @@ public class TeacherControl {
     @Test
     public void test01() {
         Teacher t = new Teacher();
-        // 解决冲突 yxy 你好
+        // 解决冲突 yxy 你好冲突
         t.setAge(12);
         t.setName("jim");
         System.out.println(t.getAge());
@@ -24,7 +24,6 @@ public class TeacherControl {
 
     @Test
     public void test02() {
-        // 解决冲突 yxy
         // 解决冲突 yxy
         Teacher t = new Teacher("tom",13);
         logger.info("============="+t.getName());
