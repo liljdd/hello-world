7f075d4a1189f60489a9a68c906ac7db3a2249e6
lilj <lilj@mchina.cn>
2018-05-25 13:45:22 +0800

    test git conflict

--- src/com/demo/TeacherControl.java
+++ src/com/demo/TeacherControl.java
@@ -3,18 +3,29 @@ package com.demo;
 import com.bean.Teacher;
 import org.junit.Test;
 
+import java.util.logging.Logger;
+
 /**
  * @author lilj
  * @date 18/05/25
  */
 public class TeacherControl {
+    public static final Logger logger = Logger.getLogger("TeacherControl");
 
     @Test
-    public void test01(){
+    public void test01() {
         Teacher t = new Teacher();
         t.setAge(12);
         t.setName("jim");
         System.out.println(t.getAge());
         System.out.println(t.toString());
+    }
+
+    @Test
+    public void test02() {
+        Teacher t = new Teacher("tom",13);
+        logger.info("============="+t.getName());
+        logger.info("============="+t.getAge());
+        System.out.println("============"+t.toString());
     }
 }
